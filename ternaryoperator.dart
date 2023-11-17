3def23029500ed3073286bad6e028c708ed250f1
Adonias <adoniashaile1@gmail.com>
2023-11-17 03:16:26 -0800

    ternary expression

---
+++ ternaryoperator.dart
@@ -0,0 +1,11 @@
+void main() {
+//ternary operator
+/*It's a shorthand way of writing an if-else statement. */
+
+//condition ? expression1 : expression2
+
+  int age1 = 23;
+  int age2 = 23;
+  String result = age1 == age2 ? " equal" : "not equal";
+  print(result);
+}
