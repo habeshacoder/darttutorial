0dbf2c6cf2598c2afba5103d8e2bcda6be518e8f
Adonias <adoniashaile1@gmail.com>
2023-11-17 01:15:56 -0800

    type system

--- anatomyofdart.dart
+++ anatomyofdart.dart
@@ -1,5 +1,4 @@
 void main() {
-
   print('Wellcome to MiniDevCamp');
   
 }
---
+++ typesindart.dart
@@ -0,0 +1,28 @@
+void main() {
+  //types in dart:
+
+  //Type Inference
+  var age = 23; //interger
+
+  //Explicit Type Annotations:
+  int age1 = 23;
+  String name = "mini";
+  //Static Type Checking /error checked at compile time
+  String name1 = "minidevcamp";
+  //Null Safety:
+  //Nullable Types
+  String name2 = "minidevccamp";
+  print(name2);
+  //Non-Nullable Types
+  String? name3;
+  print(name3);
+  //Late Modifier
+  late String name4;
+  name4 = "sample";
+  print(name4);
+  //comments in dart
+  //
+  
+
+ 
+}
